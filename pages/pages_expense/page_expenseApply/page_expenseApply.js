e382de2fa32d1d726cdee5cae96ceddf64ede44f
raggedboy <raggedboy@163.com>
2021-08-22 19:46:41 +0800

    2021年年常更新

--- pages/pages_expense/page_expenseApply/page_expenseApply.js
+++ pages/pages_expense/page_expenseApply/page_expenseApply.js
@@ -140,7 +140,7 @@ Page({
     }
 
     if (this.data.typeIndex == 1){
-      dic_submit['costTime']='2020-09-06';
+      dic_submit['costTime']='2021-08-29';
     }
     console.log('最终提交数据：', dic_submit);
     this.doCostAdd(dic_submit);
